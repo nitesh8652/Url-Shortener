79ed50ecb375c09bb96e4ef9507de6be0b701333
nitesh8652 <niteshsalian8652085233>
2025-06-25 19:30:27 +0530

    updating

--- Backend/app.js
+++ Backend/app.js
@@ -21,7 +21,7 @@ const app = express();
 // app.use(cors())
 
 app.use(cors({
-    origin:'https://ulinktrees.netlify.app',
+    origin:'https://url-shortener-frontend-d7rv.onrender.com',
     credentials: true,
        methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With']
