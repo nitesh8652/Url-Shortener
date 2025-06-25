c4ac5ee660f15c8f980ac87ce51bfa4d32bf3a87
nitesh8652 <niteshsalian8652085233>
2025-06-25 22:14:27 +0530

    initial

--- Backend/app.js
+++ Backend/app.js
@@ -21,7 +21,7 @@ const app = express();
 // app.use(cors())
 
 app.use(cors({
-    origin:'https://url-shortener-frontend-d7rv.onrender.com',
+    origin:'https://url-shortener-frontend-ving.onrender.com',
     credentials: true,
        methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With']
