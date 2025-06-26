d8e7fc372a767e4f05684bff48f76771270b5f97
nitesh8652 <niteshsalian8652085233>
2025-06-26 21:47:44 +0530

    initai;

--- Backend/app.js
+++ Backend/app.js
@@ -48,6 +48,6 @@ app.use(errorHandler)
 
 app.listen(3000, () => {
     connectDB();
-    console.log(`Server is running on port ${PORT}`);
+    console.log(`Server is running on port hehe`);
 });
 
