e8cc6859f9cc16b20efbccfd5ca59cceee09ed4a
nitesh8652 <niteshsalian8652085233>
2025-06-28 23:06:26 +0530

    front-issue-env-4

--- Frontend/src/Api/ShortUrl.Api.js
+++ Frontend/src/Api/ShortUrl.Api.js
@@ -18,6 +18,6 @@ import axiosInstance from '../Utils/Axios'
 
 export const Fetchapi = async (data) =>{
   const response = await axiosInstance.post("/api/create", data);
-  const code = response.data.shortUrl.split('/').pop();
+//   const code = response.data.shortUrl.split('/').pop();
   return code;  // now your front end will get "jHU3-c3" only
 };
