5584623c5d4d895342f3cc3a7b7dd1033920f627
nitesh8652 <niteshsalian8652085233>
2025-06-28 21:35:09 +0530

    front-issue-env-4

--- Frontend/src/Api/ShortUrl.Api.js
+++ Frontend/src/Api/ShortUrl.Api.js
@@ -7,11 +7,17 @@ import axiosInstance from '../Utils/Axios'
 //      return response.data.shortUrl;
 // }
 
-export const Fetchapi = async (data) => {
+// export const Fetchapi = async (data) => {
+//   const response = await axiosInstance.post("/api/create", data);
+//   // response.data.shortUrl is something like "https://.../jHU3-c3"
+//   // we extract just the code after the last slash:
+//   const full = response.data.shortUrl;
+//   const code = full.substring(full.lastIndexOf("/") + 1);
+//   return code;  // now your front end will get "jHU3-c3" only
+// };
+
+export const Fetchapi = async (data) =>{
   const response = await axiosInstance.post("/api/create", data);
-  // response.data.shortUrl is something like "https://.../jHU3-c3"
-  // we extract just the code after the last slash:
-  const full = response.data.shortUrl;
-  const code = full.substring(full.lastIndexOf("/") + 1);
+  const code = response.data.shortUrl.split('/').pop();
   return code;  // now your front end will get "jHU3-c3" only
 };
