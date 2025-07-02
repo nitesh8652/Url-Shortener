f0b1ec3605913fa4ddd40710a1a8196cf67bc998
nitesh8652 <niteshsalian8652085233>
2025-07-02 22:47:09 +0530

    logout issue

--- Frontend/src/Components/Form.jsx
+++ Frontend/src/Components/Form.jsx
@@ -74,7 +74,7 @@ const Form = () => {
 
   // Show loading until auth check completes
   if (loading) {
-    return <div>Checking authentication...</div>;
+    return <div className="text-center bg-green-500 text-white px-4 py-2 rounded  ">Checking authentication...</div>;
   }
 
   return (
