c5a9d3b80605f332ae7739f4e441edead8adda2f
nitesh8652 <niteshsalian8652085233>
2025-07-04 20:57:54 +0530

    Mail

--- Backend/src/Utils/Mail.js
+++ Backend/src/Utils/Mail.js
@@ -1,4 +1,4 @@
-const nodemailer = require("nodemailer");
+import nodemailer from "nodemailer";
 
 // Create a test account or replace with real credentials.
 const transporter = nodemailer.createTransport({
