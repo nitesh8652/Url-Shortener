44e8ed21d5908b899b3b25de0d58d81e1bbeaf2d
nitesh8652 <niteshsalian8652085233>
2025-07-05 21:24:24 +0530

    otp page

--- Frontend/src/Pages/DashboardPage.jsx
+++ Frontend/src/Pages/DashboardPage.jsx
@@ -2,15 +2,25 @@ import React from 'react'
 import Form from '../Components/Form.jsx'
 import UrlHistory from '../Components/UrlHistory.jsx'
 
+import { useSelector } from "react-redux";
+import { useNavigate } from "@tanstack/react-router";
+
 const DashboardPage = () => {
+  const auth = useSelector((state) => state.auth);
+  const navigate = useNavigate();
+
+  React.useEffect(() => {
+    if (!auth.user) {
+      navigate({ to: "/loginpage" });
+    }
+  }, [auth.user, navigate]);
+
   return (
     <>
-    
-    <Form />
-    <UrlHistory />
-  
+      <Form />
+      <UrlHistory />
     </>
-  )
-}
+  );
+};
 
 export default DashboardPage
