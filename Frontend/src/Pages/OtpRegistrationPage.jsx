b52c503d45b3693084faa69d61fd81a7c6441e88
nitesh8652 <niteshsalian8652085233>
2025-07-06 15:23:49 +0530

    otp verify

--- Frontend/src/Pages/OtpRegistrationPage.jsx
+++ Frontend/src/Pages/OtpRegistrationPage.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import { useRouter } from '@tanstack/react-router'
 import OtpRegistrationForm from '../Components/OtpRegistrationForm'
+import { useSelector } from 'react-redux'
 
 const OtpRegistrationPage = () => {
   const router = useRouter()
   // pull 'email' out of whatever state you passed at navigation time
-  const email = router.state?.search?.email || ''
+  const email =useSelector(state=> state.auth.userEmail || "") 
 
   return (
     <div className="min-h-screen flex flex-col items-center justify-center bg-gray-50 p-6">
